a88b57a28cf81bea8507661e49ee7d6e19af1411
IceRed0991 <wernergutschow@hotmail.com>
2023-11-08 21:14:42 -0300

    Update

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: "postgres",
   host: "192.168.5.57",
   username: "postgres",
-  password: "secret",
-  database: "teste-dominando-nodejs",
+  password: "postgres",
+  database: "develop",
   define: {
     timestamp: true, // cria duas colunas: createdAt updateAt
     underscored: true, // nomeclatura _ (não camelcase) customersGroup => customer_group
