f5c9ebda8f083d58364f2a18e35559bf5ed2cf39
IceRed0991 <wernergutschow@hotmail.com>
2023-11-14 17:43:51 -0300

    update

---
+++ src/app/playground.js
@@ -0,0 +1,7 @@
+import "./database";
+
+import Customer from "./app/models/Customer";
+
+class Playground {
+  static async play() {}
+}
--- src/database/migrations/20231109175627-create-contacts.js
+++ src/database/migrations/20231109175627-create-contacts.js
@@ -1,7 +1,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("contact", {
+    await queryInterface.createTable("contacts", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -36,6 +36,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable("users");
+    await queryInterface.dropTable("contacts");
   },
 };
